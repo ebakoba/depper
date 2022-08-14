7aa65d7d69389b75ecee02e26cfa4f4dbc0d0f9a
Kaarel Allemann <kaarelallemann@gmail.com>
2022-08-14 21:49:31 +0300

    add basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,49 @@
+//! [![github]](https://github.com/ebakoba/depper)&ensp;[![crates-io]](https://crates.io/crates/depper)&ensp;[![docs-rs]](https://docs.rs/depper)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K
+//!
+//! <br>
+//!
+//! Library for detecting dependency cycles and finding missing dependencies. It also allows to sort
+//! dependencies into tranches, which can be used as a hierarchy dependency resolution.
+//!
+//!
+//! <br>
+//!
+//! # Details
+//!
+//! - It exposes two structs `DependencyBuilder` and `Dependencies`. First is for building up the list of dependencies
+//! and building (calling the `.build()` function also validates the entire list) the second struct. Second is for
+//! generating tranches of dependencies for deployment hierarchies.
+//!
+//!
+//!   ```
+//!   use depper::Dependencies;
+//!
+//!   fn main() {
+//!     let mut dependencies_builder = Dependencies::builder()
+//!       .add_element("b", &["d"])
+//!       .add_element("c", &["d"])
+//!       .add_element("a", &["d", "e", "y"])
+//!       .add_element("d", &["e"])
+//!       .add_element("e", &[])
+//!       .add_element("y", &[]);
+//!     
+//!     // Calling the `.build()` function validates the list of dependencies.
+//!     let dependencies = dependencies_builder.build().unwrap();
+//!    
+//!    // The `.tranches()` function returns a list of tranches.
+//!     println!("{:?}", dependencies.generate_tranches().unwrap());
+//!   }
+//!   ```
+//!
+//!   ```console
+//!   [["e", "y"], ["d"], ["b", "c", "a"]]
+//!   ```
+//!
+
 use anyhow::{Ok, Result};
 use petgraph::{
     algo::is_cyclic_directed,
